f4817b95d498a8f2ad560bf44086d8818e40a6d4
Gustav Clavin <gustav.clavin99@gmail.com>
2022-09-30 13:39:07 +0200

    Completed?

--- app.js
+++ app.js
@@ -6,18 +6,26 @@ const lastResult = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
-// const countAnumber =
+const playAgain = document.querySelector('.playAgain');
+let counterDisplay = document.querySelector('.counter-display');
+let count = 0;
+guesses.textContent = 'Previous guesses: '
+
+
+function restart(){
+  location.reload();
+}
 
 function checkGuess() {
   const userGuess = Number(guessField.value);
-
-  guesses.textContent += userGuess + ' ';
+  
+  guesses.textContent += userGuess + ', ';
 
   if (userGuess == randomNumber) {
     // rätt gissningskod här
     lastResult.textContent = 'You are right! You read my mind!';
     lastResult.style.backgroundColor = 'green';
-    gameOver();
+    
   } else {
     // fel gissningskod här
     lastResult.textContent = 'Wrong!';
@@ -29,5 +37,8 @@ function checkGuess() {
     }
   }
   guessField.value = '';
+  count ++;
+  counterDisplay.innerHTML ="Total guesses: " + count;
 }
 guessSubmit.addEventListener('click', checkGuess);
+playAgain.addEventListener('click', restart)
