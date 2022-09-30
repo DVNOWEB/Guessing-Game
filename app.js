95382b3ad655c4f6f99606ac50ee46e5d927f2f8
Gustav Clavin <gustav.clavin99@gmail.com>
2022-09-30 14:45:44 +0200

    FÄRDIG

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 const playAgain = document.querySelector('.playAgain');
 let counterDisplay = document.querySelector('.counter-display');
-let count = 0;
+let count= 0;
 guesses.textContent = 'Previous guesses: '
 
 
@@ -19,25 +19,39 @@ function restart(){
 function checkGuess() {
   const userGuess = Number(guessField.value);
   
-  guesses.textContent += userGuess + ', ';
+  if (userGuess < 1  || userGuess > 100){
+    lastResult.textContent = 'You need to submit a number 1-100!';
+    lastResult.style.backgroundColor = 'red';
+  }
 
-  if (userGuess == randomNumber) {
+  else if (userGuess == randomNumber) {
     // rätt gissningskod här
     lastResult.textContent = 'You are right! You read my mind!';
     lastResult.style.backgroundColor = 'green';
-    
-  } else {
+    guesses.textContent += userGuess + ', ';
+    count ++;
+  } else if (userGuess >= 1 && userGuess <= 100) {
     // fel gissningskod här
     lastResult.textContent = 'Wrong!';
     lastResult.style.backgroundColor = 'red';
+    guesses.textContent += userGuess + ', ';
+
     if (userGuess > randomNumber) {
       lowOrHi.textContent = 'Your guess was too high';
     } else if (userGuess < randomNumber) {
       lowOrHi.textContent = 'Your guess was too low';
     }
+    count ++;
   }
+  else {
+    lastResult.textContent = 'Please enter a NUMBER 1-100';
+    lastResult.style.backgroundColor = 'red';
+  }
+
+  
+  
   guessField.value = '';
-  count ++;
+  
   counterDisplay.innerHTML ="Total guesses: " + count;
 }
 guessSubmit.addEventListener('click', checkGuess);
