cbe0b3ff4b33fb5155eeecc502778efaf8188836
Dano Zaganjor <dvno@live.se>
2022-10-02 09:57:58 +0200

    Ändring i css p.lastResult

--- app.js
+++ app.js
@@ -2,72 +2,57 @@ const randomNumber = Math.floor(Math.random() * 100) + 1;
 
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
 
+const lowOrHi = document.querySelector('.lowOrHi');
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
+const playAgain = document.querySelector('.playAgain');
+let counterDisplay = document.querySelector('.counter-display');
+let count= 0;
+guesses.textContent = 'Previous guesses: '
 
-let guessCount = 1; // 
-let resetButton;
 
-function checkGuess(){
-	const userGuess = Number(guessField.value);
-	if (guessCount == 1){
-		guesses.textContent = 'Previous guesses: ';
-	}
-	guesses.textContent += userGuess + ', ';
-	
-	if (userGuess === randomNumber){
-		lastResult.textContent = 'Congratulations! You got it right!';
-		lastResult.style.backgroundColor = 'green';
-		lowOrHi.textContent = '';
-		setGameOver();
-	}
+function restart(){
+  location.reload();
+}
+
+function checkGuess() {
+  const userGuess = Number(guessField.value);
   
-  else if (guessCount == 10){
-		lastResult.textContent = '!!!GAME OVER!!!';
-		setGameOver();
-	}
+  if (userGuess < 1  || userGuess > 100){
+    lastResult.textContent = 'You need to submit a number 1-100!';
+    lastResult.style.backgroundColor = 'red';
+  }
+
+  else if (userGuess == randomNumber) {
+    // rätt gissningskod här
+    lastResult.textContent = 'You are right! You read my mind!';
+    lastResult.style.backgroundColor = 'green';
+    guesses.textContent += userGuess + ', ';
+    count ++;
+  } else if (userGuess >= 1 && userGuess <= 100) {
+    // fel gissningskod här
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+    guesses.textContent += userGuess + ', ';
+
+    if (userGuess > randomNumber) {
+      lowOrHi.textContent = 'Your guess was too high';
+    } else if (userGuess < randomNumber) {
+      lowOrHi.textContent = 'Your guess was too low';
+    }
+    count ++;
+  }
+  else {
+    lastResult.textContent = 'Please enter a NUMBER 1-100';
+    lastResult.style.backgroundColor = 'red';
+  }
+
   
-  else{
-		lastResult.textContent = 'Wrong!';
-		lastResult.style.backgroundColor = 'red';
-		if (userGuess < randomNumber){
-			lowOrHi.textContent = 'Last guess was too low!';
-		}else if (userGuess > randomNumber){
-			lowOrHi.textContent = 'Last guess was too high!';
-		}
-	}
-	guessCount++;
-	guessField.value = '';
-	guessField.focus();
+  
+  guessField.value = '';
+  
+  counterDisplay.innerHTML ="Total guesses: " + count;
 }
 guessSubmit.addEventListener('click', checkGuess);
-
-function setGameOver(){
-	guessField.disabled = true;
-	guessSubmit.disabled = true;
-	resetButton = document.createElement('button');
-	resetButton.textContent = 'Start new game';
-	document.body.appendChild(resetButton);
-	resetButton.addEventListener('click', resetGame);
-}
-
-function resetGame(){
-	guessCount = 1;
-	
-	const resetParas = document.querySelectorAll('.resultParas p');
-	for (let i = 0; i < resetParas.length; i++){
-		resetParas[i].textContent = '';
-	}
-	
-	resetButton.parentNode.removeChild(resetButton);
-	
-	guessField.disabled = false;
-	guessSubmit.disabled = false;
-	guessField.value = '';
-	guessField.focus();
-	
-	lastResult.style.backgroundColor = 'white';
-	randomNumber = Math.floor(Math.random() * 100) + 1;
-}
+playAgain.addEventListener('click', restart)
