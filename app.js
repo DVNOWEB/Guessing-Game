701600efc4d1c453ec735134435104782c0f285c
Dano Zaganjor <dvno@live.se>
2022-10-06 14:44:49 +0200

    complet

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
-
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
-
-const lowOrHi = document.querySelector('.lowOrHi');
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 const playAgain = document.querySelector('.playAgain');
@@ -22,44 +19,34 @@ function checkGuess() {
   const userGuess = Number(guessField.value);
 
   if (userGuess < 1 || userGuess > 100) {
-    document.getElementById('smile').innerHTML = '🫣';
-    document.getElementById('myLuck').innerHTML = 'Oops not number, try again!';
-    lastResult.textContent = 'You need to submit a number 1-100!';
-    lastResult.style.backgroundColor = 'red';
+    output('🫣', 'Oops, try a valid number!', 'You need to submit a number 1-100!', 'red');
   } else if (userGuess == randomNumber) {
     // rätt gissningskod här
-    lastResult.textContent = 'You are right! You read my mind!';
-    document.getElementById('myLuck').innerHTML = 'You are amazing!';
-    document.getElementById('smile').innerHTML = '🥳';
-    lastResult.style.backgroundColor = 'green';
+    output('🥳', 'You are amazing!', 'You are right! You read my mind!', 'green');
     guesses.textContent += userGuess + ', ';
     count++;
   } else if (userGuess >= 1 && userGuess <= 100) {
     // fel gissningskod här
-    document.getElementById('myLuck').innerHTML = 'Unlucky, try again!';
-    document.getElementById('smile').innerHTML = '🤪';
-    lastResult.textContent = 'Wrong!';
-    lastResult.style.backgroundColor = 'red';
+    output('🤪', 'Unlucky, try again!', 'Wrong!', 'red');
     guesses.textContent += userGuess + ', ';
 
     if (userGuess > randomNumber) {
-      document.getElementById('myLuck').innerHTML = 'Too high! You are almost there';
-      document.getElementById('smile').innerHTML = '🙃';
-      lowOrHi.textContent = 'Your guess was too high';
+      output('🙃', 'Too high!', 'Your guess was too high', 'red');
     } else if (userGuess < randomNumber) {
-      document.getElementById('myLuck').innerHTML = `Too low! It's starting to get hot here`;
-      document.getElementById('smile').innerHTML = '😅';
-      lowOrHi.textContent = 'Your guess was too low';
+      output('😅', `Too low!`, 'Your guess was too low', 'red');
     }
     count++;
-  } else {
-    lastResult.textContent = 'Please enter a NUMBER 1-100';
-    lastResult.style.backgroundColor = 'red';
   }
-
   guessField.value = '';
   guessField.focus();
   counterDisplay.innerHTML = 'Total guesses: ' + count;
 }
 guessSubmit.addEventListener('click', checkGuess);
 playAgain.addEventListener('click', restart);
+
+const output = (smile, myLuck, message, color) => {
+  document.getElementById('smile').innerHTML = smile;
+  document.getElementById('myLuck').innerHTML = myLuck;
+  lastResult.textContent = message;
+  lastResult.style.backgroundColor = color;
+};
